cf9514f62c0a529eecc38f16158ea3dfdaaacbc5
Zach <zacharyjrgirard@hotmail.ca>
2020-04-01 02:05:37 +0000

    Header.html

---
+++ templates/index.php
@@ -0,0 +1,14 @@
+<?php include 'header.html';?>
+
+
+<body>
+    <h1>WELCOME TO TOADHUT!</h1>
+        <p> This is the landing page for ToadHut! If you have any questions please revert to our
+            discord.</p> </br>
+        <hr { display: block; height: 1px; border: 0; border-top: 1px solid #ccc; margin: 1em 0; padding: 0; }>
+        <a href="https://panel.toadhut.club" class="button">Panel</a>
+        <a href="https://discord.gg/m8cWvNB" class="button2">Discord</a>
+        <a href="legalpage.html.tera" class="button2">Legal Page</a>
+        
+</body>
+
